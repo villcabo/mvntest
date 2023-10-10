e8aef2ffd8d79be13a736c1f35dfda87e71ad386
Bismarck Villca Soliz <bismarck.villca@gmail.com>
2023-10-10 14:42:00 -0400

    End of day
    Signed-off-by: Bismarck Villca Soliz <bismarck.villca@gmail.com>

--- src/main/java/io/github/villcab/mvntest/date/StartAndEndDateFromDate.java
+++ src/main/java/io/github/villcab/mvntest/date/StartAndEndDateFromDate.java
@@ -1,9 +1,9 @@
 package io.github.villcab.mvntest.date;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAccessor;
 
 public class StartAndEndDateFromDate {
 
@@ -19,10 +19,13 @@ public class StartAndEndDateFromDate {
         ZonedDateTime startDate = start.atStartOfDay(ZoneId.systemDefault());
         ZonedDateTime endDate = end.atStartOfDay(ZoneId.systemDefault()).withHour(23).withMinute(59).withSecond(59);
 
-//        System.out.println("StarDate: " + startDate.format(DATE_TIME_FORMATTER));
-//        System.out.println("EndDate:  " + endDate.format(DATE_TIME_FORMATTER));
+        System.out.println("StarDate: " + startDate.format(DATE_TIME_FORMATTER));
+        System.out.println("EndDate:  " + endDate.format(DATE_TIME_FORMATTER));
 
-        System.out.println(LocalDate.now().minusDays(1).atStartOfDay(ZoneId.systemDefault()).format(DATE_TIME_FORMATTER));
+//        System.out.println(LocalDate.now().minusDays(1).atStartOfDay(ZoneId.systemDefault()).format(DATE_TIME_FORMATTER));
+
+        ZonedDateTime endOfDay = LocalTime.MAX.atDate(LocalDate.now()).atZone(ZoneId.systemDefault());;
+        System.out.println(endOfDay.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
 
     }
 }
